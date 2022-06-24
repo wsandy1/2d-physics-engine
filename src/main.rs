caf498b172eefd1998d2d27e5ed360321767e96d
William <mrsandywilly1@gmail.com>
2022-06-24 12:19:46 +0100

    simulation units react to resize

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
-use glutin_window::GlutinWindow as Window;
+use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, ResizeEvent, ResizeArgs};
 use piston::window::WindowSettings;
+use piston::Window;
+use graphics::math::*;
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
+
+// here, 1 unit = 1 metre
+const NUM_UNITS: u64 = 10;
+
+struct PhysicsSolver {
+    gravity: f64,
+    unit_size: f64,
+    objects: Vec<PhysicsObject>,
 }
 
-impl App {
+impl PhysicsSolver {
+    fn new(gravity: f64, size: piston::window::Size) -> Self {
+        let unit_size = size.width / NUM_UNITS as f64;
+        return Self { gravity, unit_size, objects: vec![]};
+    }
+
+    fn resize(&mut self, size: &piston::ResizeArgs) {
+        let unit_size = size.window_size[0] / NUM_UNITS as f64;
+        self.unit_size = unit_size;
+    }
+}
+
+struct PhysicsObject {
+    position_current: Vec2d<f64>,
+    position_old: Vec2d<f64>,
+    acceleration: Vec2d<f64>,
+}
+
+pub struct GraphicsInterface {
+    gl: GlGraphics, // OpenGL drawing backend.
+    solver: PhysicsSolver,
+}
+
+impl GraphicsInterface {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::{*, types::*};
 
@@ -34,8 +65,9 @@ impl App {
         });
     }
 
-    fn update(&mut self, args: &UpdateArgs) {
-
+    fn resize(&mut self, args: &ResizeArgs) {
+        self.solver.resize(args);
+        println!("{}", self.solver.unit_size);
     }
 }
 
@@ -44,26 +76,31 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create a Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", [200, 200])
+    let mut window: GlutinWindow = WindowSettings::new("spinning-square", [200, 200])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .resizable(true)
         .build()
         .unwrap();
 
-    // Create a new game and run it.
-    let mut app = App {
+    let mut gi = GraphicsInterface {
         gl: GlGraphics::new(opengl),
+        solver: PhysicsSolver::new(9.8, window.size()),
     };
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
-            app.render(&args);
+            gi.render(&args);
         }
 
-        if let Some(args) = e.update_args() {
-            app.update(&args);
+        // if let Some(args) = e.update_args() {
+        //     app.update(&args);
+        // }
+
+        if let Some(args) = e.resize_args() {
+            gi.resize(&args);
         }
+
     }
 }

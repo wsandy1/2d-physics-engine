531cd3960b09b9e616793ecff0e5700a73d7bddc
William Sandford <williamsandford@Alexs-MacBook-Air.local>
2023-06-26 10:25:18 +0100

    capitalised PointForce struct fields

--- pointforce.go
+++ pointforce.go
@@ -3,6 +3,6 @@ package main
 import "github.com/wsandy1/2d-physics-engine/vector2"
 
 type PointForce struct {
-	origin vector2.Vec2 // where force is coming from
-	dirmag vector2.Vec2 // where force is going
+	Origin vector2.Vec2 // where force is coming from
+	DirMag vector2.Vec2 // where force is going
 }

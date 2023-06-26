94c1bd6d165d3653b70954d41275775491016a09
William Sandford <williamsandford@Alexs-MacBook-Air.local>
2023-06-26 10:28:28 +0100

    added poly field and renamed vertices until I can separate graphics code

--- rigidbody.go
+++ rigidbody.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/wsandy1/2d-physics-engine/vector2"
+import (
+	"github.com/wsandy1/2d-physics-engine/graphics"
+	"github.com/wsandy1/2d-physics-engine/vector2"
+)
 
 // rigid body with positions and rotations for verlet integration
 // centre of mass is at {0,0}
@@ -15,7 +18,8 @@ type RigidBody struct {
 	rotation      float32
 	last_rotation float32
 
-	vertices     []vector2.Vec2
+	//vertices     []vector2.Vec2
+	poly         graphics.Polygon
 	point_forces []PointForce
 }
 

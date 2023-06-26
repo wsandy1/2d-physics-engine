1a25595e425922d54c2f10a1ccde1e1313d153aa
William <mrsandywilly1@gmail.com>
2023-06-26 17:50:54 +0100

    added magnitude function

--- vector2/vector2.go
+++ vector2/vector2.go
@@ -50,3 +50,8 @@ func ConstDiv(v1 Vec2, c float32) Vec2 {
 
 	return Vec2{v1.X / c, v1.Y / c}
 }
+
+// calculate magnitude of a vector
+func Magnitude(v Vec2) float32 {
+	return float32(math.Sqrt(math.Pow(float64(v.X), 2) + math.Pow(float64(v.Y), 2)))
+}

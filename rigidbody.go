2990f51c4d63dafb779b4f48a6faa963262afec7
William Sandford <williamsandford@Alexs-MacBook-Air.local>
2023-06-26 10:22:16 +0100

    renamed rigidbody struct and separated into own file

---
+++ rigidbody.go
@@ -0,0 +1,24 @@
+package main
+
+import "github.com/wsandy1/2d-physics-engine/vector2"
+
+// rigid body with positions and rotations for verlet integration
+// centre of mass is at {0,0}
+type RigidBody struct {
+	mass float32
+	moi  float32 // moment of inertia
+
+	current_position vector2.Vec2
+	last_position    vector2.Vec2
+
+	acceleration  vector2.Vec2
+	rotation      float32
+	last_rotation float32
+
+	vertices     []vector2.Vec2
+	point_forces []PointForce
+}
+
+func (b *RigidBody) Accelerate(v vector2.Vec2) {
+	b.acceleration = vector2.Add(b.acceleration, v)
+}

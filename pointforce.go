68eca3a5a3fa80f84014ac5298aca98c54f7647a
William <mrsandywilly1@gmail.com>
2023-06-29 15:28:30 +0100

    renamed DirMag property -> Vector

--- pointforce.go
+++ pointforce.go
@@ -4,5 +4,5 @@ import "github.com/wsandy1/2d-physics-engine/vector2"
 
 type PointForce struct {
 	Origin vector2.Vec2 // where force is coming from
-	DirMag vector2.Vec2 // where force is going
+	Vector vector2.Vec2 // where force is going
 }
